495c41730627a69f10539616307ca1e4617fc051
Andrea Jimenez <andrea.jimenez@unosquare.com>
2020-04-15 17:35:34 -0500

    Adding information: Work experience and Contact

--- my-page/src/app/work-experience/work-experience.component.ts
+++ my-page/src/app/work-experience/work-experience.component.ts
@@ -9,10 +9,22 @@ import { WorkExperience } from '../work_experience';
 export class WorkExperienceComponent implements OnInit {
 
   workExperience: WorkExperience = {
+  title: 'Work experience',
+  company1: 'Unosquare',
+  job_position1: 'Trainee Software Developer',
+  date1: 'Ene 2020 - Actualmente',
+  description1: '',
 
-  };
+  company2: 'Maskot',
+  job_position2: 'Trainee Software Developer',
+  date: 'Ene 2020 - Now',
+  description2:  '',
 
-
+  company3: 'Other',
+  job_position3: 'Trainee Software Developer',
+  date2: 'Ene 2020 - Now',
+  description3: '',
+  }
 
   constructor() { }
 
