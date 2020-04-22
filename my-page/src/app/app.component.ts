02e633490d8da1883cd1c38c83e29afa1fdc5ef6
Andrea Jimenez <andrea.jimenez@unosquare.com>
2020-04-22 12:39:18 -0500

    Wrapping in a grid list

--- my-page/src/app/app.component.ts
+++ my-page/src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import langSpan from '../assets/i18n/es.json';
 import langEng from '../assets/i18n/en.json';
 
+export interface Tile {
+  cols: number;
+  rows: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -12,11 +16,9 @@ export class AppComponent {
   title = 'my-page';
   currentLang = 'ES';
   texts: any = langSpan;
-
   constructor() {
     console.log(this.texts);
   }
-
   onLanguageChange() {
     console.log(this.texts);
     if (this.currentLang === 'ES') {
